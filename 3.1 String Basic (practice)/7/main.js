f6e475365fd0d41f819d1c962c2e7938ed67bd50
RusProgger <ruslantimka92@gmail.com>
2025-10-16 01:11:55 +0300

    Решение 7 задачи

--- 3.1 String Basic (practice)/7/main.js
+++ 3.1 String Basic (practice)/7/main.js
@@ -1,2 +1,9 @@
 'use strict'
 
+const name = prompt("Введите любое слово: ").trim()
+
+    if(name == "") {
+        document.write("Вы ничего не ввели")
+    } else {
+        document.write(name)
+    }
