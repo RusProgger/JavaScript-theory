cf294dbf68d4d864c057318a3744c9c8ac9b936b
RusProgger <ruslantimka92@gmail.com>
2025-10-17 13:28:50 +0300

    Решение 8 задачи, добавлена проверка

--- 3.1 String Basic (practice)/8/main.js
+++ 3.1 String Basic (practice)/8/main.js
@@ -1,2 +1,12 @@
 let text = prompt("Введите любое слово: ").trim()
-document.write(text[text.length - 1])
+
+// Проверка если пользователь ничего не введет
+
+if(text.length === 0) {
+    document.write("Вы ничего не ввели")
+    console.log("В ничего не ввели.")
+} else {
+    document.write(text[text.length - 1])
+    console.log(text[text.length - 1])
+}
+
