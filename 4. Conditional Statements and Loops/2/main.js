7ee14c78362ecb68f94a23d6cea24270c283a760
RusProgger <ruslantimka92@gmail.com>
2025-10-18 22:01:26 +0300

    Решение 2 задачи, и добавлен вывод на страницу

--- 4. Conditional Statements and Loops/2/main.js
+++ 4. Conditional Statements and Loops/2/main.js
@@ -6,6 +6,8 @@ const number = prompt("Введите число: ").trim()
 
 if(number % 2 === 0) {
     console.log("Число четное ", number)
+    document.write("Число чётное: ", number)
 } else {
     console.log("Число нечетное ", number)
+    document.write("Число нечетное ", number)
 }
