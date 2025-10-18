33fd1826aca278ca2d1e9c07826843e0958ccf15
RusProgger <ruslantimka92@gmail.com>
2025-10-18 08:52:13 +0300

    Решение 9 задачи, добавлены условия

--- 3.1 String Basic (practice)/9/main.js
+++ 3.1 String Basic (practice)/9/main.js
@@ -1,7 +1,29 @@
-let textInput = prompt("Введите имя: ").trim().split('').reverse().join('')
+'use strict'
 
-document.write(`Строка в обратном порядке: ${textInput}`)
-console.log(`Строка в обратном порядке: ${textInput}`)
+let inputText = prompt("Введите строку: ")
+console.log(`Вы ввели строку: ${inputText}`)
+
+// проверка 
+
+if(inputText !== null && inputText.trim() !== '') {
+    let reverse = inputText.split('').reverse().join('')
+    // вывод 
+    document.write(`Строка в обратном порядке: ${reverse}`)
+    console.log(`Строка в обратном порядке: ${reverse}`)
+} else {
+    document.write(`Вы ничего не ввели`)
+    console.log(`Вы ничего не ввели`)
+}
+
+
+
+// let textInput = prompt("Введите имя: ").trim().split('').reverse().join('')
+
+// document.write(`Строка в обратном порядке: ${textInput}`)
+// console.log(`Строка в обратном порядке: ${textInput}`)
+
+
+
 
 
 /* 
