bedcd7210c6d1510d9ab1fe3d9d4a993978334fc
RusProgger <ruslantimka92@gmail.com>
2025-10-20 01:52:40 +0300

    Добавлено решение 4 задачи

--- 4. Conditional Statements and Loops/4/main.js
+++ 4. Conditional Statements and Loops/4/main.js
@@ -1,2 +1,14 @@
 'use strict'
 
+/*
+    ### ✅ Задание 4: Счёт от 1 до 10
+    - Используй цикл `for`
+    - Выведи числа от 1 до 10 построчно
+*/
+
+let number = 10
+
+for(let i = 1; i <= number; i++) {
+    document.body.innerHTML  += `Счет ${i}<br>`
+    console.log(i)
+}
