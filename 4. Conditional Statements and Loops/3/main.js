0af604d834279aec1f8d7ab68ec47a281485c9c6
RusProgger <ruslantimka92@gmail.com>
2025-10-19 16:42:10 +0300

    Решение 3 задачи, добавлен вывод на страницу

--- 4. Conditional Statements and Loops/3/main.js
+++ 4. Conditional Statements and Loops/3/main.js
@@ -14,14 +14,18 @@ let score = prompt("Введите оценку: ").trim()
 
 if(score >= 90) {
     console.log("Отлично!")
+    document.write(`Ваша оценка ${score} и это Отлично!`)
 }
 else if(score >= 70 && score < 90) {
     console.log("Хорошо!")
+    document.write(`Ваша оценка ${score} и это Хорошо!`)
 }
 
 else if(score >= 50 && score < 70) {
     console.log("Удовлетворительно!")
+    document.write(`Ваша оценка ${score} и это Удовлетворительно!`)
 }
 else {
     console.log("Плохо!")
+    document.write(`Ваша оценка ${score} и это Плохо!`)
 }
