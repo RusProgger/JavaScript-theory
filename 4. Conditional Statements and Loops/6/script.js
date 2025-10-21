517e62b97c5f6467be2862079a1bafc558deb58c
RusProgger <ruslantimka92@gmail.com>
2025-10-21 12:28:05 +0300

    Решение 6 задачи

--- 4. Conditional Statements and Loops/6/script.js
+++ 4. Conditional Statements and Loops/6/script.js
@@ -1,2 +1,21 @@
 'use strict'
 
+// Начало таблицы цикл
+
+let table = 100
+for(let a = 0; a < table; a++) {
+    document.body.innerHTML += `-`
+    console.log("-")
+}
+
+document.body.innerHTML += `<br>Таблица умножения `
+
+for(let i = 3; i <= 3; i++) {
+
+    for(let j = 1; j <= 10; j++) {
+        console.log(` ${i} * ${j} = ${i * j}`)
+        document.body.innerHTML += `<br>${i} * ${j} = ${i * j} `
+    }
+
+    document.body.innerHTML += `<br>------------------------<br>`;
+}
