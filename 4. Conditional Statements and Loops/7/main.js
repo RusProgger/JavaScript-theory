b0ae0c466142ae1291b2f6eaad23b78f5e8383f9
RusProgger <ruslantimka92@gmail.com>
2025-10-21 12:50:22 +0300

    Добавлено решение 7 задачи с циклами

--- 4. Conditional Statements and Loops/7/main.js
+++ 4. Conditional Statements and Loops/7/main.js
@@ -1,8 +1,20 @@
 'use strict'
 
-let number = Number(prompt("Введите число: ").trim());
+// let number = Number(prompt("Введите число: ").trim());
 
-let summa = number * (number + 1) / 2
+// let summa = number * (number + 1) / 2
+
+// document.body.innerHTML += `Сумма чисел = ${summa}`
+
+
+
+//  Версия 2 на циклах 
+
+let number = Number(prompt("Введите число: ").trim()) 
+let summa = 0
+
+for(let i = 1; i <= number; i++) {
+    summa += i
+}
 
 document.body.innerHTML += `Сумма чисел = ${summa}`
-
