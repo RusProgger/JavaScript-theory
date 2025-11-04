c615866764ce89987936dfb438d74a4f362af039
RusProgger <ruslantimka92@gmail.com>
2025-11-04 22:18:42 +0200

    Работаем с функциями в языке JS

--- 5. Function/function.js
+++ 5. Function/function.js
@@ -22,5 +22,15 @@ const nameUser = function(userName) {
 
 console.log(nameUser("Denis"));
 
-document.body.innerHTML += `${nameUser("Denis")}`
+document.body.innerHTML += `${nameUser("Denis")} <br>`
+
+
+// напишем функцию которая будет принимать возраст и имя
+
+function userInfo(nameUsers, ageUser) {
+
+    return `Привет ${nameUsers}, тебе ${ageUser} лет`
+}
+
+document.body.innerHTML += `${userInfo("Ivan", 30)}`
 
