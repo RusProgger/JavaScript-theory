5dbcf26449d7dfa5b25745a2eae8a3d095c57404
RusProgger <ruslantimka92@gmail.com>
2025-11-04 21:51:59 +0200

    Добавлен console.log для вывода информации

--- 3.1 String Basic (practice)/4/main.js
+++ 3.1 String Basic (practice)/4/main.js
@@ -1,3 +1,5 @@
 const name = prompt("Введите имя: ")
 
 document.write(`Ваше имя ${name} и оно содержит ${name.length} символов`)
+
+console.log(`Ваше имя ${name} и оно содержит ${name.length} символов`)
