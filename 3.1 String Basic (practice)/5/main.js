28403ce007e742901bc11746ca73656dc71a7b75
RusProgger <ruslantimka92@gmail.com>
2025-11-04 21:55:05 +0200

    Добавлен console.log для вывода информации

--- 3.1 String Basic (practice)/5/main.js
+++ 3.1 String Basic (practice)/5/main.js
@@ -2,6 +2,8 @@ const name = prompt("Введите ваше имя: ")
 
 if(name.trim().toLowerCase() === "admin") {
     document.write(`Добро пожаловать, админ!`)
+    console.log("Добро пожаловать, админ!")
 } else {
     document.write(`Привет, пользователь!`)
+    console.log("Привет, пользователь!")
 }
