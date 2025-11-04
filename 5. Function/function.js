426c404dd2d26def52620d0d85c8d670797096d2
RusProgger <ruslantimka92@gmail.com>
2025-11-04 22:03:59 +0200

    Работаем с функциями в языке JS

--- 5. Function/function.js
+++ 5. Function/function.js
@@ -1,2 +1,12 @@
 'use strict'
 
+// Обьявление функции (Function Declaration)
+
+function name(name) {
+    return `Привет, ${name}`
+}
+
+// вывод функции
+console.log(name("Alex"));
+
+document.body.innerHTML += `${name("Alex")}`
