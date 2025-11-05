f24dcf410c6e859d279dae35655a0ac3686d4005
RusProgger <ruslantimka92@gmail.com>
2025-11-05 14:54:09 +0200

    Решение 2 задачи

---
+++ 5. Function/task function/2/script.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/*
+
+    Проверка на чётность
+
+    Напиши функцию isEven(num), которая возвращает true, если число чётное,
+    и false, если нечётное.
+
+*/
+
+function isEven(num) {
+
+    return (num % 2 === 0);
+}
+
+console.log(isEven(5))
