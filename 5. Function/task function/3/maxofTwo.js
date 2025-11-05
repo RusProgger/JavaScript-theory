d3688cf60115b0769479637d450572d32b0ebff6
RusProgger <ruslantimka92@gmail.com>
2025-11-05 15:03:41 +0200

    Решение 3 задачи

--- 5. Function/task function/3/maxofTwo.js
+++ 5. Function/task function/3/maxofTwo.js
@@ -1,2 +1,19 @@
 'use strict'
 
+function maxOfTwo(a, b) {
+
+    if(a > b) {
+        return a
+    }
+
+    else if(a == b) {
+        return `Числа равны`
+    }
+    
+    else {
+        return b
+    }
+}
+
+console.log(maxOfTwo(10, 15))
+console.log(maxOfTwo(10, 10))
