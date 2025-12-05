678c19c0374e3c0f4d71301404d2aaf5318213aa
RusProgger <ruslantimka92@gmail.com>
2025-12-05 19:49:30 +0200

    Обьекты в JS

--- 7. Object/main.js
+++ 7. Object/main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+// создаем обьект с именем Car 
+
+const car = {
+    name: "BMW",
+    age: 2001,
+};
+
+// консольный вывод 
+console.log(`Название модели машины: ${car.name}\nГод выпуска ${car.age}`);
