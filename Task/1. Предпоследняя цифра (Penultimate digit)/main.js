6b5f6a2d5d49de9f2db77936f77000551ad40a06
RusProgger <ruslantimka92@gmail.com>
2026-05-15 13:13:14 +0300

    Add task Предпоследняя цифра (Penultimate digit)

--- Task/1. Предпоследняя цифра (Penultimate digit)/main.js
+++ Task/1. Предпоследняя цифра (Penultimate digit)/main.js
@@ -2,6 +2,10 @@
 
 let number = parseInt(prompt("Введите число: "))
 
+// проверка условий
+
 let result = Math.floor(Math.abs(number) / 10) % 10
 
-console.log("Вывод: ${result}")
+console.log(`Вывод: ${result}`)
+
+document.body.textContent = `Вывод: ${result}`
