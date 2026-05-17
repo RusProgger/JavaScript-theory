773d47735028ea4bfbb186074e4f155c4e6ac2e4
RusProgger <ruslantimka92@gmail.com>
2026-05-17 23:59:05 +0300

    Практика работа со строками в JS

--- 3.1 String Basic (practice)/4/main.js
+++ 3.1 String Basic (practice)/4/main.js
@@ -1,5 +1,5 @@
 const name = prompt("Введите имя: ").trim()
 
-document.write(`Ваше имя ${name} и оно содержит ${name.length} символов`)
+// document.write(`Ваше имя ${name} и оно содержит ${name.length} символов`)
 
 console.log(`Ваше имя ${name} и оно содержит ${name.length} символов`)
