2a954a4b5ef8bbe8ffa53eb4dd5642abb7d36b01
RusProgger <ruslantimka92@gmail.com>
2026-05-19 14:25:34 +0300

    Вывод через input

--- Task/2. Вывод через input/main.js
+++ Task/2. Вывод через input/main.js
@@ -4,4 +4,6 @@ const input = document.querySelector("#text")
 const button = document.querySelector(".submit_btn")
 const result = document.querySelector(".result")
 
-
+button.addEventListener("click", function() {
+    result.textContent = input.value;
+})
