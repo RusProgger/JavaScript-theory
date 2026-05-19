a3820dacb1d892c98e0eeabd3b27028594c9756b
RusProgger <ruslantimka92@gmail.com>
2026-05-19 15:39:02 +0300

    Вывод информации через JS

--- Task/2. Вывод через input/main.js
+++ Task/2. Вывод через input/main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+// Вариант первый когда в реьд разметке уже есть пустой тег
 const input = document.querySelector("#text")
 const button = document.querySelector(".submit_btn")
 const result = document.querySelector(".result")
@@ -7,3 +7,6 @@ const result = document.querySelector(".result")
 button.addEventListener("click", function() {
     result.textContent = input.value;
 })
+
+// Второй вариант более универсальный без добавления пустого тега на html
+
