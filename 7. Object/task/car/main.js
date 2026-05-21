b807ae8be4acb65d578d9c4937e73cb469de4604
RusProgger <ruslantimka92@gmail.com>
2026-05-21 16:53:18 +0300

    Практика JS - обьекты

--- 7. Object/task/car/main.js
+++ 7. Object/task/car/main.js
@@ -1,2 +1,20 @@
 'use strict'
 
+const Car = [
+    {
+        name: "BMW",
+        color: "black",
+        year: 2015
+},
+
+{
+    name: "Audi",
+    color: "red",
+    year: 2011
+},
+
+{
+    
+}
+
+]
